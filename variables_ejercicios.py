b3bb318f733e3739a946eb528fdd1db1aa641b4e
ahixa-mejia <142328844+ahixa-mejia@users.noreply.github.com>
2023-08-29 20:11:24 -0600

    variables_python
    se realizo el ejercicio de Codelab: variables python

---
+++ variables_ejercicios.py
@@ -0,0 +1,29 @@
+#variables 
+
+Nombre = "rubi"
+edad = 23
+estatura = 1.57
+es_alta = True
+es_baja = False
+#concatenar
+Nombre = + edad 
+str(input('nombre: '))
+int(input('edad: '))
+print ("ingresa tu estatura" )
+float(input('estatura: '))
+if estatura >= 1.65:
+  print ("¡eres una persona alta!")
+else:
+  print ("¡eres una persona bajita!")
+
+#enteros y floats
+#python tiene enteros sin limites mientras que los float, no.
+#la Precisión en float es fija, en Decimal es variable.
+
+print ("¡hagamos una suma simple, ingresa dos numeros!")
+
+n1 = int(input('Número 1: '))
+n2 = int(input('Número 2: '))
+print('La suma de los dos números es: ', n1+n2)
+
+#terminamosejercicio.
